a250c11f47e0281241609107bf83b9ee88ee9e14
KygenHack <148578932+KygenHack@users.noreply.github.com>
2023-11-26 23:59:29 +0100

    Update NavBar.tsx

--- src/components/dashboard/NavBar.tsx
+++ src/components/dashboard/NavBar.tsx
@@ -42,11 +42,11 @@ export default function Navbar() {
     {/* Navbar */}
     <div className="w-full navbar">
       <div className="flex-1 px-2 mx-2">
-      <img
+      {/* <img
           src="https://www.billionlocalcoin.io/public/images/logo.png"
           alt="Logo"
           width="50px"
-        />
+        /> */}
         <div className="title text-portal">
         <h4>Welcome Back!</h4>
         <h1>
