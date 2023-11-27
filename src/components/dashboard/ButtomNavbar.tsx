db88d127ecbf60db61a6f32b62ac0fbc2cc4a83a
KygenHack <148578932+KygenHack@users.noreply.github.com>
2023-11-27 08:49:52 +0100

    Update ButtomNavbar.tsx

--- src/components/dashboard/ButtomNavbar.tsx
+++ src/components/dashboard/ButtomNavbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const ButtomNavbar = () => {
   return (
-    <div className="btm-nav">
+    <div className="btm-nav bg-portal">
         <NavLink to="/smart-portal" className="">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
