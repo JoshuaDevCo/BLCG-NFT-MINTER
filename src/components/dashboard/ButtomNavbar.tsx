d0e0ecd5ab25ee5e56802395d367611f86ae68b4
KygenHack <148578932+KygenHack@users.noreply.github.com>
2023-12-03 17:59:24 +0100

    Update ButtomNavbar.tsx

--- src/components/dashboard/ButtomNavbar.tsx
+++ src/components/dashboard/ButtomNavbar.tsx
@@ -5,9 +5,9 @@ const ButtomNavbar = () => {
   return (
     <div className="btm-nav bg-portal">
       <NavLink to="/" className="">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house h-5 w-5" viewBox="0 0 16 16">
+  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+</svg>
         <span className="btm-nav-label">Home</span>
       </NavLink>
 
