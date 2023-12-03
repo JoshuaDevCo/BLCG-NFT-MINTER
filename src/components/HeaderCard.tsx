4447254f10df268f74abdc94b20073248e93b474
KygenHack <148578932+KygenHack@users.noreply.github.com>
2023-12-03 15:30:22 +0100

    update

--- src/components/HeaderCard.tsx
+++ src/components/HeaderCard.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const HeaderCard = () => {
   return (
-    <div className="trcHeader px-4 py-3 text-white">
+    <div className="trcHeader px-4 py-3 text-black">
     <p className="text-center headertext font-medium">
-    <a target="_blank" href="http://accounts.thexprotocol.com/"> <h1>🚀 TheX ICO: Your Gateway to Upcoming Web3 Dominance! ⌛ Don't Miss Out. 🌐 <span>#JoinNow</span></h1></a>
+    <i>BLCG IDO: Get Entry! ⌛ Don't Miss Out.<a className="rainbow-blink-btn" target="_blank" href="https://ico.billionlocalcoin.net">🌐 #JoinNow!</a></i>
     </p>
   </div>
   )
